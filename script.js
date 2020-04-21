e96b220d788523d83f8b4bae3ff6091ae8a2d61b
Caitlyn <cgreffly@gmail.com>
2020-04-20 21:51:42 -0700

    adding typed.js

--- script.js
+++ script.js
@@ -1,81 +1,49 @@
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  // console.log({ i });
-  var fullTxt = this.toRotate[i];
-  // console.log({ fullTxt });
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else if (this.txt === 'app.' || 'campaign.') {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  } else {
-    this.txt = 'brand.';
-  }
-
-  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) {
-    delta /= 2;
-  }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta / 2);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('textRotate');
-  for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    console.log('length: ', toRotate.valueOf());
-    var period = elements[i].getAttribute('data-period');
-    console.log('period: ', period);
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      console.log(JSON.parse(toRotate));
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement('style');
-  css.type = 'text/css';
-  css.innerHTML = '.textRotate > .wrap { border-right: 0.08em solid #666 }';
-  document.body.appendChild(css);
-};
+import Typed from 'typed.js';
 
 const navItems = document.querySelectorAll('.navItem');
+const typed = document.querySelectorAll('.typed');
 
 $('nav')
   .find('.pageLink')
-  .click(function(e) {
+  .click(function (e) {
     // menuButton.classList.remove('close');
     // menu.classList.remove('show');
     // menuNav.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
+    navItems.forEach((item) => item.classList.remove('show'));
     showMenu = false;
     let section = $(this).attr('href');
     $('html, body').animate({
-      scrollTop: $(section).offset().top - 75
+      scrollTop: $(section).offset().top - 75,
     });
   });
+
+// typewriter effect
+var options = {
+  strings: ['app.', 'campaign.', 'video.', 'movement.', 'site.', 'brand.'],
+  // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+  // stringsElement: null,
+  // typing speed
+  typeSpeed: 30,
+  // time before typing starts
+  startDelay: 1200,
+  // backspacing speed
+  backSpeed: 20,
+  // time before backspacing
+  backDelay: 500,
+  // loop
+  loop: true,
+  // false = infinite
+  loopCount: 1,
+  // show cursor
+  showCursor: false,
+  // character for cursor
+  cursorChar: '|',
+  // attribute to type (null == text)
+  attr: null,
+  // either html or text
+  contentType: 'html',
+};
+
+var typed = new Typed('.typed', options);
+
+$(typed);
