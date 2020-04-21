8068008c895ebfe7e51b5b796d36672f637cbdb7
Caitlyn <cgreffly@gmail.com>
2020-04-21 10:18:29 -0700

    trying typed.js again

--- script.js
+++ script.js
@@ -18,32 +18,38 @@ $('nav')
   });
 
 // typewriter effect
-var options = {
-  strings: ['app.', 'campaign.', 'video.', 'movement.', 'site.', 'brand.'],
-  // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-  // stringsElement: null,
-  // typing speed
-  typeSpeed: 30,
-  // time before typing starts
-  startDelay: 1200,
-  // backspacing speed
-  backSpeed: 20,
-  // time before backspacing
-  backDelay: 500,
-  // loop
-  loop: true,
-  // false = infinite
-  loopCount: 1,
-  // show cursor
-  showCursor: false,
-  // character for cursor
-  cursorChar: '|',
-  // attribute to type (null == text)
-  attr: null,
-  // either html or text
-  contentType: 'html',
-};
-
-var typed = new Typed('.typed', options);
-
-$(typed);
+$(function () {
+  $('.typed').typed({
+    strings: ['app.', 'campaign.', 'video.', 'movement.', 'site.', 'brand.'],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 30,
+    // time before typing starts
+    startDelay: 1200,
+    // backspacing speed
+    backSpeed: 20,
+    // time before backspacing
+    backDelay: 500,
+    // loop
+    loop: true,
+    // false = infinite
+    loopCount: 1,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: '|',
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html',
+    // call when done callback function
+    callback: function () {},
+    // starting callback function before each string
+    preStringTyped: function () {},
+    //callback for every typed string
+    onStringTyped: function () {},
+    // callback for reset
+    resetCallback: function () {},
+  });
+});
